17c179259894c56cc30e883cb10fbae34334e551
SiongSng <rrt467778@gmail.com>
2023-03-12 18:22:25 +0800

    feat: about page

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,9 @@ import 'package:cap_countdown/pages/countdown_page.dart';
 import 'package:cap_countdown/pages/exam.dart';
 import 'package:cap_countdown/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 final ConfigInstance config = ConfigInstance();
 
@@ -68,12 +71,48 @@ class _MainPageState extends State<MainPage> {
           ),
           actions: [
             PopupMenuButton(
-              itemBuilder: (context) => const [
-                PopupMenuItem(
+              itemBuilder: (context) => [
+                const PopupMenuItem(
                   child: Text('設定'),
                 ),
                 PopupMenuItem(
-                  child: Text('關於'),
+                  child: const Text('關於'),
+                  onTap: () async {
+                    await Future.delayed(const Duration(milliseconds: 200));
+                    // ignore: use_build_context_synchronously
+                    if (!context.mounted) return;
+
+                    showAboutDialog(
+                        context: context,
+                        applicationName: '會考倒數',
+                        applicationVersion: '1.0.0+1',
+                        children: [
+                          const Text(
+                              '這是個具有試題等豐富功能的會考倒數計時器\n此外，手機版如果覺得看題目的圖片太小，可以直接用手去縮放，像是 Instagram 一樣。'),
+                          const Divider(),
+                          const Text(
+                              '本應用程式的程式碼皆採用 GNU General Public License v3.0 (GPL-3.0) 授權條款釋出。'),
+                          const Text(
+                              '此外，本應用程式中所採用的國中教育會考歷屆試題版權皆為 國中教育會考全國試務會 與 國立臺灣師範大學心理與教育測驗研究發展中心 擁有，且本應用程式與上述兩個單位無任何關係。'),
+                          const Divider(),
+                          Row(
+                            children: [
+                              const Text('開發者: '),
+                              const Text('SiongSng（菘菘）'),
+                              IconButton(
+                                  onPressed: () {
+                                    launchUrlString(
+                                        'https://github.com/SiongSng/cap-countdown');
+                                  },
+                                  icon: const Icon(LineIcons.github))
+                            ],
+                          ),
+                          const Text(
+                              'Copyright © 2023 SiongSng All rights reserved.'),
+                        ],
+                        applicationIcon: Image.asset('assets/images/logo.png',
+                            width: 40, height: 40));
+                  },
                 ),
               ],
             ),
