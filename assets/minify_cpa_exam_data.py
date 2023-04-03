092dd8d2cdb6f7248a15b2f375399bae25d1c2a1
SiongSng <rrt467778@gmail.com>
2023-04-03 15:23:13 +0800

    fix(assets): try windows
    Signed-off-by: SiongSng <rrt467778@gmail.com>

--- assets/minify_cpa_exam_data.py
+++ assets/minify_cpa_exam_data.py
@@ -1,14 +1,16 @@
 import json
 
+
 def minify_json(file_path: str) -> str:
-    with open(file_path, "r") as f:
-        data = json.load(f)
+    with open(file_path, "r", encoding="utf8") as file:
+        data = json.load(file)
         return json.dumps(data, ensure_ascii=False, separators=(",", ":"))
+
 
 # Minify the `cap_exams.json` file to reduce its size.
 if __name__ == "__main__":
-     minified_data = minify_json("cap_exams.json")
-     with open("cap_exams.json", "w") as f:
+    minified_data = minify_json("cap_exams.json")
+    with open("cap_exams.json", "w") as f:
         f.write(minified_data)
 
-     print("Successfully minified the `cap_exams.json` file.")
+    print("Successfully minified the `cap_exams.json` file.")
