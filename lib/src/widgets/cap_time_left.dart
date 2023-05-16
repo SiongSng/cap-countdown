894a30936c1a625a8c4a651d20c8f7f207bb632a
SiongSng <rrt467778@gmail.com>
2023-05-16 22:56:37 +0800

    Update cap_time_left.dart

--- lib/src/widgets/cap_time_left.dart
+++ lib/src/widgets/cap_time_left.dart
@@ -18,7 +18,6 @@ class _CAPTimeLeftState extends State<CAPTimeLeft> {
   void initState() {
     super.initState();
 
-    if (!widget.showDetail) return;
     Timer.periodic(const Duration(seconds: 1), (timer) {
       if (!mounted) return timer.cancel();
       setState(() {});
@@ -27,8 +26,11 @@ class _CAPTimeLeftState extends State<CAPTimeLeft> {
 
   @override
   Widget build(BuildContext context) {
-    final duration = CAPUtil.getDurationToCAP();
-    final day = getDay(duration);
+    // final duration = CAPUtil.getDurationToCAP();
+
+    const duration = Duration(hours: 10, minutes: 59, seconds: 59);
+    final capIsAboutToStart = duration.inDays == 0;
+    final capIsStarted = duration.inMilliseconds == 0;
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -37,82 +39,94 @@ class _CAPTimeLeftState extends State<CAPTimeLeft> {
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              Text(
-                '距離會考只剩下',
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              RichText(
-                  text: TextSpan(children: [
-                TextSpan(
-                  text: day.toString(),
-                  style: TextStyle(
-                      fontSize: 85.5,
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white
-                          : Colors.black),
+              if (capIsAboutToStart)
+                Text(
+                  '加油！堅持到最後一刻～',
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                      color: Theme.of(context).colorScheme.secondary),
                 ),
-                TextSpan(
-                  text: '天',
+              if (!capIsAboutToStart)
+                Text(
+                  '距離會考只剩下',
                   style: Theme.of(context).textTheme.titleLarge,
-                )
-              ])),
-              if (widget.showDetail)
-                Wrap(spacing: 5, children: [
-                  RichText(
-                      text: TextSpan(children: [
-                    TextSpan(
-                      text: (duration.inHours % 24).toString(),
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black),
-                    ),
-                    TextSpan(
-                      text: '小時',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    )
-                  ])),
-                  RichText(
-                      text: TextSpan(children: [
-                    TextSpan(
-                      text: (duration.inMinutes % 60).toString(),
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black),
-                    ),
-                    TextSpan(
-                      text: '分鐘',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    )
-                  ])),
-                  RichText(
-                      text: TextSpan(children: [
-                    TextSpan(
-                      text: (duration.inSeconds % 60).toString(),
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black),
-                    ),
-                    TextSpan(
-                      text: '秒鐘',
-                      style: Theme.of(context).textTheme.titleLarge,
-                    )
-                  ])),
-                ])
+                ),
+              if (widget.showDetail || duration.inDays > 0)
+                RichText(
+                    text: TextSpan(children: [
+                  TextSpan(
+                    text: getDay(duration).toString(),
+                    style: TextStyle(
+                        fontSize: 85.5,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black),
+                  ),
+                  TextSpan(
+                    text: '天',
+                    style: Theme.of(context).textTheme.titleLarge,
+                  )
+                ])),
+              if (widget.showDetail || capIsAboutToStart)
+                _buildDetails(duration, context)
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildDetails(Duration duration, BuildContext context) {
+    return Wrap(spacing: 5, children: [
+      RichText(
+          text: TextSpan(children: [
+        TextSpan(
+          text: (duration.inHours % 24).toString(),
+          style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? Colors.white
+                  : Colors.black),
+        ),
+        TextSpan(
+          text: '小時',
+          style: Theme.of(context).textTheme.titleLarge,
+        )
+      ])),
+      RichText(
+          text: TextSpan(children: [
+        TextSpan(
+          text: (duration.inMinutes % 60).toString(),
+          style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? Colors.white
+                  : Colors.black),
+        ),
+        TextSpan(
+          text: '分鐘',
+          style: Theme.of(context).textTheme.titleLarge,
+        )
+      ])),
+      RichText(
+          text: TextSpan(children: [
+        TextSpan(
+          text: (duration.inSeconds % 60).toString(),
+          style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).brightness == Brightness.dark
+                  ? Colors.white
+                  : Colors.black),
+        ),
+        TextSpan(
+          text: '秒鐘',
+          style: Theme.of(context).textTheme.titleLarge,
+        )
+      ])),
+    ]);
   }
 
   int getDay(Duration duration) {
