b7dd3f3a04904be9ef5a643493bdfa8ce77434b2
菘菘 <rrt467778@gmail.com>
2023-05-23 23:14:29 +0800

    feat: 113 CAP exam

--- lib/src/config/config_instance.dart
+++ lib/src/config/config_instance.dart
@@ -2,8 +2,8 @@ import 'package:cap_countdown/src/config/config_helper.dart';
 import 'package:pub_semver/pub_semver.dart';
 
 class ConfigInstance {
-  DateTime get capStartTime => DateTime.fromMillisecondsSinceEpoch(
-      ConfigHelper.get<int>('cap_start_time'));
+  DateTime get capStartTime =>
+      DateTime.parse(ConfigHelper.get<String>('cap_start_time'));
 
   Version get appVersion =>
       Version.parse(ConfigHelper.get<String>('app_version'));
