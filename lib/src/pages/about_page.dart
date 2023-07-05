e5eee150978b925105031615c66828afd60c358a
菘菘 <rrt467778@gmail.com>
2023-07-05 17:53:00 +0800

    feat: 112 chinese exam data

--- lib/src/pages/about_page.dart
+++ lib/src/pages/about_page.dart
@@ -45,7 +45,7 @@ class AboutPage extends StatelessWidget {
                 children: [
                   MarkdownBody(
                       data:
-                          '- 本應用程式的程式碼皆採用 [GNU General Public License v3.0 (GPL-3.0)](https://www.gnu.org/licenses/gpl-3.0.html) 授權條款釋出。\n- 本應用程式內的國中教育會考歷屆試題（含聽力音檔）皆來自於 [國中教育會考全國試務會](https://cap.rcpet.edu.tw/NEAC/About/About) 與 [國立臺灣師範大學心理與教育測驗研究發展中心](https://rcpet.edu.tw/)，且本應用程式與上述兩個單位無任何關係。\n- 本應用程式內的國中教育會考歷屆試題的部份詳解來自於 [甄戰一點通](https://www.reallygood.com.tw/newExam/inside?str=F7229F6ADD9F230A5FB41A782B73F2A0)，且本應用程式與上述單位無任何關係。',
+                          '- 本應用程式的程式碼皆採用 [GNU General Public License v3.0 (GPL-3.0)](https://www.gnu.org/licenses/gpl-3.0.html) 授權條款釋出。\n- 本應用程式內的國中教育會考歷屆試題（含聽力音檔）皆來自於 [國中教育會考全國試務會](https://cap.rcpet.edu.tw/NEAC/About/About) 與 [國立臺灣師範大學心理與教育測驗研究發展中心](https://rcpet.edu.tw/)，且本應用程式與上述兩個單位無任何關係。\n- 本應用程式內的國中教育會考歷屆試題的部份詳解來自於 [甄戰一點通](https://www.reallygood.com.tw/newExam/inside?str=F7229F6ADD9F230A5FB41A782B73F2A0)，其餘則由開發者自行撰寫，且本應用程式與上述單位無任何關係。',
                       onTapLink: (text, href, title) {
                         if (href == null) return;
                         launchUrlString(href);
