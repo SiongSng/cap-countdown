78cf3123b66f2e340dc83321140a08623570f983
菘菘 <rrt467778@gmail.com>
2023-07-27 22:07:09 +0800

    feat: 0.2.1 changelog

--- lib/src/widgets/changelog_dialog.dart
+++ lib/src/widgets/changelog_dialog.dart
@@ -15,7 +15,7 @@ class ChangelogDialog extends StatelessWidget {
           children: [
             MarkdownBody(
                 data:
-                    '## 有什麼新鮮事？\n### 歡迎您使用會考沙漏 **${config.appVersion}** 版～  \n### 本次更新共包含以下內容：\n\n* 新增 112 年會考題庫\n* 支援更多的題目顯示模式，使作答體驗更為逼真\n* 在模擬歷屆試題介面中新增「數學參考公式」供參考\n* 壓縮題庫資源檔，減少安裝所需的空間\n* 修復翻轉螢幕時每日一題中的題目會被重置\n* 改善部份題目的圖片位置，避免遺漏掉資訊\n* 改善「準備模擬歷屆試題」的介面佈局'),
+                    '## 有什麼新鮮事？\n### 歡迎您使用會考沙漏 **${config.appVersion}** 版～  \n### 本次更新共包含以下內容：\n\n* 新增 110 數學科題庫與詳解\n* 修正 112 自然科部份題目之參考答案誤值\n* 修正部份使用者回報的題目錯誤\n* Google Play Store 中加入平板裝置宣傳圖'),
             const SizedBox(height: 12),
             TextButton(
               onPressed: () {
