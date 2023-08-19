a5377a79950bf98902b9a0c2a8c47773cdada14e
HACO <jasonytonlinecomeandsee@gmail.com>
2023-08-19 20:37:50 +0800

    Create ci_post_install.sh

---
+++ ios/ci_scripts/ci_post_install.sh
@@ -0,0 +1,17 @@
+# Install CocoaPods using Homebrew.
+brew install cocoapods
+
+# Install Flutter
+brew install --cask flutter
+
+# Run Flutter doctor
+flutter doctor
+
+# Get packages
+flutter packages get
+
+# Update generated files
+flutter pub run build_runner build
+
+# Build ios app
+flutter build ios
