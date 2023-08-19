e4fb81ea98ec89e8bee9f9ff104c20fb6aedf422
HACO <jasonytonlinecomeandsee@gmail.com>
2023-08-19 23:15:41 +0800

    Update ci_post_clone.sh

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+echo 'export GEM_HOME=$HOME/gems' >> ~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >> ~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH=$GEM_HOME/gems/bin:$PATH
+
 # Install CocoaPods using Homebrew.
 brew install cocoapods
 
